dce1f31795d000ab876bbcf855c0992e60a056ab
Ginger Lee Kretchmer and Michael Pinaud <michael@>
2017-10-17 13:29:45 -0700

    Allow all flash cards to toggle definition from hidden to show

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(function () {
+  $("#javascript-button").click(function() {
+    $("#javascript-text").toggle();
+  });
+
+  $("#operators-button").click(function() {
+    $("#operators-text").toggle();
+  });
+
+  $("#variables-button").click(function() {
+    $("#variables-text").toggle();
+  });
+
+  $("#variable-nc-button").click(function() {
+    $("#variable-nc-text").toggle();
+  });
+
+  $("#functions-button").click(function() {
+    $("#functions-text").toggle();
+  });
+
+  $("#methods-button").click(function() {
+    $("#methods-text").toggle();
+  });
+
+  $("#arguments-button").click(function() {
+    $("#arguments-text").toggle();
+  });
+
+  $("#parameters-button").click(function() {
+    $("#parameters-text").toggle();
+  });
+
+  $("#return-button").click(function() {
+    $("#return-text").toggle();
+  });
+
+  $("#chaining-button").click(function() {
+    $("#chaining-text").toggle();
+  });
+
+  $("#strings-button").click(function() {
+    $("#strings-text").toggle();
+  });
+
+  $("#booleans-button").click(function() {
+    $("#booleans-text").toggle();
+  });
+
+  $("#undefined-button").click(function() {
+    $("#undefined-text").toggle();
+  });
+
+  $("#nan-button").click(function() {
+    $("#nan-text").toggle();
+  });
+
+  $("#escape-button").click(function() {
+    $("#escape-text").toggle();
+  });
+
+  $("#alert-button").click(function() {
+    $("#alert-text").toggle();
+  });
+
+  $("#comments-button").click(function() {
+    $("#comments-text").toggle();
+  });
+
+  $("#jquery-button").click(function() {
+    $("#jquery-text").toggle();
+  });
+
+  $("#attributes-button").click(function() {
+    $("#attributes-text").toggle();
+  });
+
+});
